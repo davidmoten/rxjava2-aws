e3e65f8ec8c068cc0b78827deaab58067919a5a0
Dave Moten <davidmoten@gmail.com>
2016-09-19 18:14:31 +1000

    add tests

--- src/test/java/com/github/davidmoten/rx/aws/SqsMessageTest.java
+++ src/test/java/com/github/davidmoten/rx/aws/SqsMessageTest.java
@@ -1,15 +1,21 @@
 package com.github.davidmoten.rx.aws;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import java.util.Optional;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class SqsMessageTest {
 
 	@Test
 	public void test() {
-		new SqsMessage("r", new byte[] {}, 1000, Optional.of("123"),
+		SqsMessage m = new SqsMessage("r", new byte[] {}, 1000, Optional.of("123"),
 				new SqsMessage.Service(null, null, null, null, null, null));
+		assertEquals(1000L, m.lastModifiedTime());
+		assertTrue(m.toString().contains("123"));
 	}
 
 }
