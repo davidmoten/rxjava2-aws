1e45f43d29b01e3f89ee179d95894979684e43a0
Dave Moten <davidmoten@gmail.com>
2016-09-19 14:41:26 +1000

    add AwsClients tests

---
+++ src/test/java/com/github/davidmoten/rx/aws/AwsClientsTest.java
@@ -0,0 +1,48 @@
+package com.github.davidmoten.rx.aws;
+
+import org.junit.Test;
+
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.github.davidmoten.junit.Asserts;
+
+public class AwsClientsTest {
+
+	@Test
+	public void tests3() {
+		AwsClients.createS3Client(new AWSCredentials() {
+
+			@Override
+			public String getAWSSecretKey() {
+				return "a";
+			}
+
+			@Override
+			public String getAWSAccessKeyId() {
+				return "b";
+			}
+		}, new ClientConfiguration(), Regions.AP_SOUTHEAST_2);
+	}
+	
+	@Test
+	public void testSqs() {
+		AwsClients.createSqsClient(new AWSCredentials() {
+
+			@Override
+			public String getAWSSecretKey() {
+				return "a";
+			}
+
+			@Override
+			public String getAWSAccessKeyId() {
+				return "b";
+			}
+		}, new ClientConfiguration(), Regions.AP_SOUTHEAST_2);
+	}
+	
+	@Test
+	public void isUtilityClass() {
+		Asserts.assertIsUtilityClass(AwsClients.class);
+	}
+}
