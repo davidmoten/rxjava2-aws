b166d8b6f17c6578d632f09bc74f3358cb5009c4
Dave Moten <davidmoten@gmail.com>
2016-09-20 12:41:53 +1000

    add tests

--- src/main/java/com/github/davidmoten/rx/aws/SqsMessage.java
+++ src/main/java/com/github/davidmoten/rx/aws/SqsMessage.java
@@ -4,6 +4,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.Optional;
 
+import com.amazonaws.AmazonWebServiceClient;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.sqs.AmazonSQSClient;
 import com.github.davidmoten.util.Preconditions;
@@ -12,108 +13,115 @@ import rx.functions.Func0;
 
 public final class SqsMessage {
 
-	private final String messageReceiptHandle;
-	private final byte[] bytes;
-	private final long timestamp;
-	private final Optional<String> s3Id;
-	private final Service service;
+    private final String messageReceiptHandle;
+    private final byte[] bytes;
+    private final long timestamp;
+    private final Optional<String> s3Id;
+    private final Service service;
 
-	SqsMessage(String messageReceiptHandle, byte[] bytes, long timestamp, Optional<String> s3Id, Service service) {
-		this.messageReceiptHandle = messageReceiptHandle;
-		this.bytes = bytes;
-		this.timestamp = timestamp;
-		this.s3Id = s3Id;
-		this.service = service;
-	}
+    SqsMessage(String messageReceiptHandle, byte[] bytes, long timestamp, Optional<String> s3Id,
+            Service service) {
+        this.messageReceiptHandle = messageReceiptHandle;
+        this.bytes = bytes;
+        this.timestamp = timestamp;
+        this.s3Id = s3Id;
+        this.service = service;
+    }
 
-	public byte[] bytes() {
-		return bytes;
-	}
-	
-	public String message() {
-		return new String(bytes(), StandardCharsets.UTF_8);
-	}
+    public byte[] bytes() {
+        return bytes;
+    }
 
-	public long lastModifiedTime() {
-		return timestamp;
-	}
+    public String message() {
+        return new String(bytes(), StandardCharsets.UTF_8);
+    }
 
-	public void deleteMessage() {
-		try {
-			deleteMessage(Client.FROM_SOURCE);
-		} catch (RuntimeException e) {
-			deleteMessage(Client.FROM_FACTORY);
-		}
-	}
+    public long lastModifiedTime() {
+        return timestamp;
+    }
 
-	public void deleteMessage(Client client) {
-		if (client == Client.FROM_SOURCE) {
-			deleteMessage(service.s3, service.sqs);
-		} else {
-			Optional<AmazonS3Client> s3 = service.s3Factory.map(Func0::call);
-			AmazonSQSClient sqs = service.sqsFactory.call();
-			try {
-				deleteMessage(s3, sqs);
-			} finally {
-				if (s3.isPresent()) {
-					try {
-						s3.get().shutdown();
-					} catch (RuntimeException e) {
-					}
-				}
-				try {
-					sqs.shutdown();
-				} catch (RuntimeException e) {
+    public void deleteMessage() {
+        try {
+            deleteMessage(Client.FROM_SOURCE);
+        } catch (RuntimeException e) {
+            deleteMessage(Client.FROM_FACTORY);
+        }
+    }
 
-				}
-			}
-		}
-	}
+    public void deleteMessage(Client client) {
+        if (client == Client.FROM_SOURCE) {
+            deleteMessage(service.s3, service.sqs);
+        } else {
+            deleteMessageUsingFactory(service.s3Factory, service.sqsFactory);
+        }
+    }
 
-	public void deleteMessage(Optional<AmazonS3Client> s3, AmazonSQSClient sqs) {
-		Preconditions.checkArgument(!s3.isPresent() || s3Id.isPresent(), "s3Id must be present");
-		if (s3Id.isPresent()) {
-			s3.get().deleteObject(service.bucketName.get(), s3Id.get());
-		}
-		sqs.deleteMessage(service.queueName, messageReceiptHandle);
-	}
+    private void deleteMessageUsingFactory(Optional<Func0<AmazonS3Client>> s3Factory,
+            Func0<AmazonSQSClient> sqsFactory) {
+        Optional<AmazonS3Client> s3 = s3Factory.map(Func0::call);
+        AmazonSQSClient sqs = sqsFactory.call();
+        try {
+            deleteMessage(s3, sqs);
+        } finally {
+            s3.ifPresent(SqsMessage::shutdown);
+            shutdown(sqs);
+        }
+    }
 
-	@Override
-	public String toString() {
-		return "MessageAndBytes [messageReceiptHandle=" + messageReceiptHandle + ", bytes=" + Arrays.toString(bytes)
-				+ ", timestamp=" + timestamp + ", s3Id=" + s3Id + ", bucketName=" + service.bucketName + ", queueName="
-				+ service.queueName + "]";
-	}
+    // visible for testing
+    static void shutdown(AmazonWebServiceClient client) {
+        try {
+            client.shutdown();
+        } catch (RuntimeException e) {
+            // ignore
+        }
+    }
 
-	public static enum Client {
-		FROM_SOURCE, FROM_FACTORY;
-	}
+    public void deleteMessage(Optional<AmazonS3Client> s3, AmazonSQSClient sqs) {
+        Preconditions.checkArgument(!s3.isPresent() || s3Id.isPresent(), "s3Id must be present");
+        if (s3Id.isPresent()) {
+            s3.get().deleteObject(service.bucketName.get(), s3Id.get());
+        }
+        sqs.deleteMessage(service.queueName, messageReceiptHandle);
+    }
 
-	static class Service {
+    @Override
+    public String toString() {
+        return "MessageAndBytes [messageReceiptHandle=" + messageReceiptHandle + ", bytes="
+                + Arrays.toString(bytes) + ", timestamp=" + timestamp + ", s3Id=" + s3Id
+                + ", bucketName=" + service.bucketName + ", queueName=" + service.queueName + "]";
+    }
 
-		final Func0<AmazonSQSClient> sqsFactory;
-		final Optional<Func0<AmazonS3Client>> s3Factory;
-		final Optional<AmazonS3Client> s3;
-		final AmazonSQSClient sqs;
-		final String queueName;
-		final Optional<String> bucketName;
+    public static enum Client {
+        FROM_SOURCE, FROM_FACTORY;
+    }
 
-		Service(Optional<Func0<AmazonS3Client>> s3Factory, Func0<AmazonSQSClient> sqsFactory, Optional<AmazonS3Client> s3,
-				AmazonSQSClient sqs, String queueName, Optional<String> bucketName) {
-			Preconditions.checkNotNull(s3Factory);
-			Preconditions.checkNotNull(sqsFactory);
-			Preconditions.checkNotNull(s3);
-			Preconditions.checkNotNull(sqs);
-			Preconditions.checkNotNull(queueName);
-			Preconditions.checkNotNull(bucketName);
-			this.s3Factory = s3Factory;
-			this.sqsFactory = sqsFactory;
-			this.s3 = s3;
-			this.sqs = sqs;
-			this.queueName = queueName;
-			this.bucketName = bucketName;
-		}
+    static class Service {
 
-	}
+        final Func0<AmazonSQSClient> sqsFactory;
+        final Optional<Func0<AmazonS3Client>> s3Factory;
+        final Optional<AmazonS3Client> s3;
+        final AmazonSQSClient sqs;
+        final String queueName;
+        final Optional<String> bucketName;
+
+        Service(Optional<Func0<AmazonS3Client>> s3Factory, Func0<AmazonSQSClient> sqsFactory,
+                Optional<AmazonS3Client> s3, AmazonSQSClient sqs, String queueName,
+                Optional<String> bucketName) {
+            Preconditions.checkNotNull(s3Factory);
+            Preconditions.checkNotNull(sqsFactory);
+            Preconditions.checkNotNull(s3);
+            Preconditions.checkNotNull(sqs);
+            Preconditions.checkNotNull(queueName);
+            Preconditions.checkNotNull(bucketName);
+            this.s3Factory = s3Factory;
+            this.sqsFactory = sqsFactory;
+            this.s3 = s3;
+            this.sqs = sqs;
+            this.queueName = queueName;
+            this.bucketName = bucketName;
+        }
+
+    }
 
 }
--- src/test/java/com/github/davidmoten/rx/aws/SqsMessageTest.java
+++ src/test/java/com/github/davidmoten/rx/aws/SqsMessageTest.java
@@ -5,54 +5,123 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.Optional;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.InOrder;
 import org.mockito.Mockito;
 
+import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.sqs.AmazonSQSClient;
 import com.amazonaws.services.sqs.model.DeleteMessageResult;
 import com.github.davidmoten.rx.aws.SqsMessage.Client;
 
 public class SqsMessageTest {
 
-	@Test
-	public void test() {
-		AmazonSQSClient sqs = Mockito.mock(AmazonSQSClient.class);
-		SqsMessage m = createMessage(sqs);
-		assertEquals(1000L, m.lastModifiedTime());
-		assertTrue(m.toString().contains("123"));
-	}
+    private static final String BUCKET = "bucket";
+    private static final String RECEIPT_HANDLE = "receiptHandle";
+    private static final String QUEUE = "queue";
 
-	private static SqsMessage createMessage(AmazonSQSClient sqs) {
-		return new SqsMessage("r", new byte[] {}, 1000, Optional.of("123"),
-				new SqsMessage.Service(Optional.empty(), () -> sqs, Optional.empty(), sqs, "queue", Optional.empty()));
-	}
+    @Test
+    public void test() {
+        AmazonSQSClient sqs = Mockito.mock(AmazonSQSClient.class);
+        SqsMessage m = createMessage(sqs);
+        assertEquals(1000L, m.lastModifiedTime());
+        assertTrue(m.toString().contains("123"));
+    }
 
-	@Test
-	public void testClientFromFactory() {
-		AmazonSQSClient sqs = Mockito.mock(AmazonSQSClient.class);
-		SqsMessage m = new SqsMessage("receiptHandle", new byte[] {}, 1000, Optional.empty(),
-				new SqsMessage.Service(Optional.empty(), () -> sqs, Optional.empty(), sqs, "queue", Optional.empty()));
-		Mockito.when(sqs.deleteMessage("queue", "receiptHandle")).thenReturn(new DeleteMessageResult());
-		m.deleteMessage(Client.FROM_FACTORY);
-		InOrder inorder = Mockito.inOrder(sqs);
-		inorder.verify(sqs, Mockito.times(1)).deleteMessage("queue", "receiptHandle");
-		inorder.verify(sqs, Mockito.times(1)).shutdown();
-		Mockito.verifyNoMoreInteractions(sqs);
-	}
+    private static SqsMessage createMessage(AmazonSQSClient sqs) {
+        return new SqsMessage("r", new byte[] {}, 1000, Optional.of("123"), new SqsMessage.Service(
+                Optional.empty(), () -> sqs, Optional.empty(), sqs, QUEUE, Optional.empty()));
+    }
 
-	@Test
-	public void testClientFromSourceFailsThenFailsOverToFromFactory() {
-		AmazonSQSClient sqs = Mockito.mock(AmazonSQSClient.class);
-		SqsMessage m = new SqsMessage("receiptHandle", new byte[] {}, 1000, Optional.empty(),
-				new SqsMessage.Service(Optional.empty(), () -> sqs, Optional.empty(), sqs, "queue", Optional.empty()));
-		Mockito.when(sqs.deleteMessage("queue", "receiptHandle")).thenThrow(new RuntimeException())
-				.thenReturn(new DeleteMessageResult());
-		m.deleteMessage();
-		InOrder inorder = Mockito.inOrder(sqs);
-		inorder.verify(sqs, Mockito.times(2)).deleteMessage("queue", "receiptHandle");
-		inorder.verify(sqs, Mockito.times(1)).shutdown();
-		Mockito.verifyNoMoreInteractions(sqs);
-	}
+    @Test
+    public void testClientFromFactory() {
+        AmazonSQSClient sqs = Mockito.mock(AmazonSQSClient.class);
+        SqsMessage m = new SqsMessage(RECEIPT_HANDLE, new byte[] {}, 1000, Optional.empty(),
+                new SqsMessage.Service(Optional.empty(), () -> sqs, Optional.empty(), sqs, QUEUE,
+                        Optional.empty()));
+        Mockito.when(sqs.deleteMessage(QUEUE, RECEIPT_HANDLE))
+                .thenReturn(new DeleteMessageResult());
+        m.deleteMessage(Client.FROM_FACTORY);
+        InOrder inorder = Mockito.inOrder(sqs);
+        inorder.verify(sqs, Mockito.times(1)).deleteMessage(QUEUE, RECEIPT_HANDLE);
+        inorder.verify(sqs, Mockito.times(1)).shutdown();
+        Mockito.verifyNoMoreInteractions(sqs);
+    }
+
+    @Test
+    public void testClientFromSourceFailsThenFailsOverToFromFactory() {
+        AmazonSQSClient sqs = Mockito.mock(AmazonSQSClient.class);
+        SqsMessage m = new SqsMessage(RECEIPT_HANDLE, new byte[] {}, 1000, Optional.empty(),
+                new SqsMessage.Service(Optional.empty(), () -> sqs, Optional.empty(), sqs, QUEUE,
+                        Optional.empty()));
+        Mockito.when(sqs.deleteMessage(QUEUE, RECEIPT_HANDLE)).thenThrow(new RuntimeException())
+                .thenReturn(new DeleteMessageResult());
+        m.deleteMessage();
+        InOrder inorder = Mockito.inOrder(sqs);
+        inorder.verify(sqs, Mockito.times(2)).deleteMessage(QUEUE, RECEIPT_HANDLE);
+        inorder.verify(sqs, Mockito.times(1)).shutdown();
+        Mockito.verifyNoMoreInteractions(sqs);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void s3IdAndS3FactoryConflictThrowsException() {
+        AmazonSQSClient sqs = Mockito.mock(AmazonSQSClient.class);
+        AmazonS3Client s3 = Mockito.mock(AmazonS3Client.class);
+        SqsMessage m = new SqsMessage(RECEIPT_HANDLE, new byte[] {}, 1000, Optional.empty(),
+                new SqsMessage.Service(Optional.empty(), () -> sqs, Optional.empty(), sqs, QUEUE,
+                        Optional.empty()));
+        m.deleteMessage(Optional.of(s3), sqs);
+    }
+
+    @Test
+    public void deleteMessageFromFactoryWhenS3FactoryExists() {
+        AmazonSQSClient sqs = Mockito.mock(AmazonSQSClient.class);
+        AmazonS3Client s3 = Mockito.mock(AmazonS3Client.class);
+        String s3Id = "123";
+        SqsMessage m = new SqsMessage(RECEIPT_HANDLE, new byte[] {}, 1000, Optional.of(s3Id),
+                new SqsMessage.Service(Optional.of(() -> s3), () -> sqs, Optional.of(s3), sqs,
+                        QUEUE, Optional.of(BUCKET)));
+        Mockito.when(sqs.deleteMessage(QUEUE, RECEIPT_HANDLE))
+                .thenReturn(new DeleteMessageResult());
+        m.deleteMessage(Client.FROM_FACTORY);
+        InOrder inorder = Mockito.inOrder(sqs, s3);
+        inorder.verify(s3, Mockito.times(1)).deleteObject(BUCKET, s3Id);
+        inorder.verify(sqs, Mockito.times(1)).deleteMessage(QUEUE, RECEIPT_HANDLE);
+        inorder.verify(s3, Mockito.times(1)).shutdown();
+        inorder.verify(sqs, Mockito.times(1)).shutdown();
+        Mockito.verifyNoMoreInteractions(sqs, s3);
+    }
+
+    @Test
+    public void shutdownDoesNotRethrowException() {
+        AmazonSQSClient sqs = Mockito.mock(AmazonSQSClient.class);
+        Mockito.doThrow(new RuntimeException()).when(sqs).shutdown();
+        SqsMessage.shutdown(sqs);
+    }
+
+    @Test
+    public void shutdownOfSqsAndS3FactoryCreatedClientsOccursWhenS3DeleteObjectFails() {
+        AmazonSQSClient sqs = Mockito.mock(AmazonSQSClient.class);
+        AmazonS3Client s3 = Mockito.mock(AmazonS3Client.class);
+        String s3Id = "123";
+        SqsMessage m = new SqsMessage(RECEIPT_HANDLE, new byte[] {}, 1000, Optional.of(s3Id),
+                new SqsMessage.Service(Optional.of(() -> s3), () -> sqs, Optional.of(s3), sqs,
+                        QUEUE, Optional.of(BUCKET)));
+        Mockito.when(sqs.deleteMessage(QUEUE, RECEIPT_HANDLE))
+                .thenReturn(new DeleteMessageResult());
+        Mockito.doThrow(new RuntimeException()).when(s3).deleteObject(BUCKET, s3Id);
+        try {
+            m.deleteMessage(Client.FROM_FACTORY);
+            Assert.fail();
+        } catch (RuntimeException e) {
+            // do nothing
+        }
+        InOrder inorder = Mockito.inOrder(sqs, s3);
+        inorder.verify(s3, Mockito.times(1)).deleteObject(BUCKET, s3Id);
+        inorder.verify(s3, Mockito.times(1)).shutdown();
+        inorder.verify(sqs, Mockito.times(1)).shutdown();
+        Mockito.verifyNoMoreInteractions(sqs, s3);
+    }
 
 }
